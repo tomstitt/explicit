03a2ff30a519d65e8872d6aafa2bc536a6835daf
Tom Stitt <tomstit@gmail.com>
2015-12-31 17:16:44 +0000

    clean out node

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 /**
- * Crowd-sourcing playlist creation
- *
- * 1. User logins in
- * 2. User selects a playlist
- * 3. User is redirected to individual page where songs
- *      are added when chosen from a search
- *      (this page is available to anyone with the url)
- *
- * Modified from:
- * https://github.com/spotify/web-api-auth-examples/tree/master/authorization_code
- * More information at:
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
+ * Minimal Node Host
+ **/
 
 var express = require('express'); // Express web server framework
-var cookieParser = require('cookie-parser');
 var port = 5556;
-var public_dir = 'public'; //process.env.CROWDIFY_PUBLIC_DIR || null;
-
+var public_dir = 'public'; 
 
 var app = express();
-app.use(express.static(__dirname + '/' + public_dir))
-    .use(cookieParser());
-
+app.use(express.static(__dirname + '/' + public_dir));
 
 console.log('Listening on ' + port);
 app.listen(port);
